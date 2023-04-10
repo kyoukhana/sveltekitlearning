ce02153e6849e71bbeb12ee645aea413d28c1a96
Kevin Youkhana <kyoukhana@gmail.com>
2023-04-09 21:13:11 -0400

    Lesson 48 - Thanks for Paying Page
    Pretty simple lesson

---
+++ src/routes/(dashboard)/invoices/thanks/+page@.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Thank You | The Dollar Holler</title>
+</svelte:head>
+
+<div class="h-screen w-screen center bg-thankYou bg-center bg-no-repat">
+    <div>
+        <h1 class="text-[3.5rem] font-black leading-none text-daisyBush">Thank You</h1>
+        <p class="text-lg text-daisyBush">Your payment has been received</p>
+    </div>
+</div>
