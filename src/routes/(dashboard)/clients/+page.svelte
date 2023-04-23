5c7458b06246cf0aa6c07e894adbf215a1d22b02
Kevin Youkhana <kyoukhana@gmail.com>
2023-04-23 15:22:46 -0400

    Lesson 53 - Styling the Blank Client State
    This one was pretty easy

--- src/routes/(dashboard)/clients/+page.svelte
+++ src/routes/(dashboard)/clients/+page.svelte
@@ -5,6 +5,7 @@
     import ClientRow from './ClientRow.svelte';
     import { clients, loadClients } from '$lib/stores/ClientStore';
     import { onMount } from 'svelte';
+	  import BlankSlate from './BlankSlate.svelte';
   
     onMount(() => {
       loadClients();
@@ -16,12 +17,13 @@
   <div
     class="md:gap-7-4 mb-7 flex flex-col-reverse items-start justify-between gap-y-6 md:flex-row md:items-center lg:mb-16"
   >
-    <!-- search field -->
-    <!-- {#if $invoices.length > 0} -->
-    <Search />
-    <!-- {:else}
+    
+  <!-- search field -->
+    {#if $clients.length > 0}
+      <Search />
+    {:else}
       <div />
-    {/if} -->
+    {/if}
   
     <!-- new invoice button -->
     <div>
@@ -34,22 +36,22 @@
     </div>
   </div>
   
-  <!-- clients -->
-  <div>
-    {#if $clients === null}
-      Loading...
-    {:else if $clients.length <= 0}
-      Blank State
-    {:else}
-      <!--  client header row -->
-      <ClientRowHeader />
-  
-      <!-- client rows -->
-      <div class="flex flex-col-reverse">
-        {#each $clients as client}
-          <ClientRow {client} />
-        {/each}
-      </div>
-    {/if}
-  </div>
+<!-- clients -->
+<div>
+  {#if $clients === null}
+    Loading...
+  {:else if $clients.length <= 0}
+    <BlankSlate />
+  {:else}
+    <!--  client header row -->
+    <ClientRowHeader />
+
+    <!-- client rows -->
+    <div class="flex flex-col-reverse">
+      {#each $clients as client}
+        <ClientRow {client} />
+      {/each}
+    </div>
+  {/if}
+</div>
   
