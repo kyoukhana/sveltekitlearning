9df3a41a39da42233bdec698f1cde8905cfc0b5a
Kevin Youkhana <kyoukhana@gmail.com>
2023-05-21 14:12:42 -0400

    Lesson 63
    Lesson 63 - Animating the Slide out Panel (New Invoice and New Client) This one is pretty easy.

--- src/lib/components/SlidePanel.svelte
+++ src/lib/components/SlidePanel.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-     import { createEventDispatcher } from 'svelte';
+	import {fly} from "svelte/transition";
+	import { createEventDispatcher } from 'svelte';
 	import Portal from '$lib/components/Portal.svelte';
 	import Overlay from './Overlay.svelte';
 	import Arrow from '$lib/components/icon/Arrow.svelte';
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 </script>
 
 <svelte:window
@@ -18,7 +19,8 @@
 	<Overlay />
 	<aside
 		class="fixed overflow-y-scroll right-0 top-0 z-slidePanel h-screen w-full lg:w-3/4 bg-white lg:py-20 lg:px-32 px-5 pt-16 shadow-slidePanel"
-	>
+	     transition:fly="{{ x:1000, duration:500 }}"
+		>
 		<button class="absolute top-5 left-7 text-pastelPurple hover:text-daisyBush" on:click={() => dispatch('closePanel')}>
 			<Arrow />
 		</button>
