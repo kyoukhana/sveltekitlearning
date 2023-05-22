52752cdaf4d81d9b1207e56586a7d71c874c22fc
Kevin Youkhana <kyoukhana@gmail.com>
2023-05-22 10:56:45 -0400

    Lesson 64
    Creating a Page Transition to View an Invoice.  Pretty simple lesson

--- src/routes/(dashboard)/invoices/[id]/+layout@.svelte
+++ src/routes/(dashboard)/invoices/[id]/+layout@.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import {fly} from "svelte/transition"
 	import { afterNavigate, goto } from '$app/navigation';
 	import Arrow from '$lib/components/icon/Arrow.svelte';
 	let previousPage: string | undefined = undefined;
@@ -17,7 +18,7 @@
 />
 
 <div class="h-full min-h-screen w-screen bg-whisper print:bg-transparent pt-16 lg:pt-12 print:pt-0 print:pb-0 pb-32">
-	<main class="mx-auto min-h-screen max-w-screen-lg">
+	<main class="mx-auto min-h-screen max-w-screen-lg" transition:fly={{y:500, duration:250}}>
 	  <a href={previousPage ? previousPage : '/invoices'} class="fixed top-7 left-7 text-pastelPurple print:hidden"
 		><Arrow /></a
 	  >
