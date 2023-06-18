4493a540a29107fbd89f361c477a22680e07d4ba
Kevin Youkhana <kyoukhana@gmail.com>
2023-06-18 16:16:55 -0400

    Revert "Lesson 72 - Set up Supabase"
    This reverts commit e412a8c16900925a7d55d4079d062db3873ccfe8.

--- src/lib/utils/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://jrskeyrnctemyrmgjohy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impyc2tleXJuY3RlbXlybWdqb2h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcxMDIxODQsImV4cCI6MjAwMjY3ODE4NH0.BBUQRgKeg6nH954D4oveXBkT37EmmY9A5EeTJnXN7eA')
-
-export default supabase;
