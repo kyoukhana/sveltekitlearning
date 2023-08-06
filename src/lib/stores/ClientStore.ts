258073579293455b24f64d3959556f5ebb071b49
Kevin Youkhana <kyoukhana@gmail.com>
2023-08-06 16:06:06 -0400

    Lesson 83 Adding a Client
    Pretty easy lesson :)

--- src/lib/stores/ClientStore.ts
+++ src/lib/stores/ClientStore.ts
@@ -1,6 +1,6 @@
 import supabase from '$lib/utils/supabase';
 import { writable } from "svelte/store";
-import data from "../../seed.json";
+import { snackbar } from "./SnackbarStore";
 
 export const clients = writable<Client[]>([]);
 
@@ -15,8 +15,26 @@ export const loadClients = async () => {
   clients.set(data as Client[]);
 }
 
-export const addClient = (clientToAdd: Client) => {
-  clients.update((prev: Client[]) => [...prev, {...clientToAdd, clientStatus: "active"}]);
+export const addClient = async (clientToAdd: Client) => {
+  const { data, error } = await supabase
+    .from('client')
+    .insert([
+      { ...clientToAdd, clientStatus: "active" },
+    ])
+    .select()
+
+  if (error) {
+    console.error(error);
+    snackbar.send({
+      message: error.message,
+      type: "error",
+    })
+    return;
+  }
+
+  const id = data[0].id;
+
+  clients.update((prev: Client[]) => [...prev, { ...clientToAdd, clientStatus: "active", id }]);
   return clientToAdd;
 }
 
@@ -26,16 +44,18 @@ export const updateClient = (clientToUpdate: Client) => {
   return clientToUpdate;
 }
 
-export const getClientById = async (id:string) =>{
-  const { data, error } = await supabase.from('client')
-  .select('*, invoice(id, invoiceStatus,invoiceNumber,dueDate, client(id, name), lineItems(*))')
-  .eq('id',id);
+export const getClientById = async (id: string) => {
+  const { data, error } = await supabase
+    .from('client')
+    .select('*, invoice(id, invoiceStatus, invoiceNumber, dueDate, client(id, name), lineItems(*))')
+    .eq('id', id);
 
-  if(error){
-    console.log(error);
+  if (error) {
+    console.error(error);
     return;
   }
 
   if (data && data[0]) return data[0] as Client;
-    console.warn('cannot find a client');
+
+  console.warn('cannot find a client');
 }
