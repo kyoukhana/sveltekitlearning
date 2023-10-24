0144fb7b1090ccc35597b4f7c94292a999ed477a
Kevin Youkhana <kyoukhana@gmail.com>
2023-10-24 07:21:14 -0400

    Lesson 93 - Handling the Logout
    Pretty simple lesson.  Keep in mind client side goto

---
+++ src/routes/(auth)/logout/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import supabase from '$lib/utils/supabase';
+	import { onMount } from 'svelte';
+    
+	onMount(async () =>  {
+		let { error } = await supabase.auth.signOut();
+
+        if(error){
+            console.log(error);
+        }
+
+        goto('/login'); //Only works client side refer lesson again for more options
+	});
+</script>
